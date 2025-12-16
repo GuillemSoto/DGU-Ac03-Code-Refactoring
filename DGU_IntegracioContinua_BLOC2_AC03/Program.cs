7b74a5f1a50fcba3a2e176cf4493805d368f313a
DiegoGrajedaR <diego.grajeda.7e8@itb.cat>
2025-12-16 12:48:34 +0100

    Update Program.cs

--- DGU_IntegracioContinua_BLOC2_AC03/Program.cs
+++ DGU_IntegracioContinua_BLOC2_AC03/Program.cs
@@ -44,19 +44,15 @@ namespace Refactor
         }
         public static void PrintResult(int result)
         {
+            //Hem separat la funció que calcula el resultat de la que el printa
             const string PositiveMsg = "El resultat és positiu.";
             const string ZeroMsg = "El resultat és zero.";
-
-            // Eliminem switch + default (branca morta)
-            if (result == 0)
+            switch (result)
             {
-                Console.Write(ZeroMsg);
-            }
-            else
-            {
-                Console.Write(PositiveMsg);
+                case 0: Console.Write(ZeroMsg); break;
+                case > 0: Console.Write(PositiveMsg); break;
+                default: break; // NOSONAR
             }
         }
-
     }
 }
